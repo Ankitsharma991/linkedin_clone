8a00a059681836c09c16b65d323109a715453e0d
Ankit Sharma <sharmaa7861@gmail.com>
2023-04-23 22:10:53 +0530

    moving to add loader to profile

--- src/components/common/ProfileCard/index.jsx
+++ src/components/common/ProfileCard/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./index.scss";
 export default function ProfileCard({ currentUser }) {
-  console.log("profileCard:", currentUser);
+  // console.log("profileCard:", currentUser);
   return <div className="profile-card">{currentUser.name}</div>;
 }
