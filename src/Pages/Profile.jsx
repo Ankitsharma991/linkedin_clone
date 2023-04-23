8b62c1b238ce913d5d2e6ced480e30a21ce6a606
Ankit Sharma <sharmaa7861@gmail.com>
2023-04-23 22:15:54 +0530

    Loader component added to profile page

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ProfileComponent from "../components/common/ProfileComponent";
 
+import { onAuthStateChanged } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import { auth } from "../firebaseConfig";
+import { Loader } from "../components/common/Loader/Loader";
+
 export default function Profile({ currentUser }) {
-//   console.log("Profile: ", currentUser);
-  return <ProfileComponent currentUser={currentUser} />;
+  const [loading, setLoading] = useState(true);
+  let navigate = useNavigate();
+  useEffect(() => {
+    onAuthStateChanged(auth, (res) => {
+      if (!res?.accessToken) {
+        navigate("/");
+      } else {
+        setLoading(false);
+      }
+    });
+  }, []);
+  return loading ? <Loader /> : <ProfileComponent currentUser={currentUser} />;
 }
