c0323c8807f67f8878f849fbc83c7e216ab7397f
Ankit Sharma <sharmaa7861@gmail.com>
2023-04-23 15:58:55 +0530

    profile card name problem fixed

--- src/components/common/ProfileCard/index.jsx
+++ src/components/common/ProfileCard/index.jsx
@@ -2,5 +2,5 @@ import React from "react";
 import "./index.scss";
 export default function ProfileCard({ currentUser }) {
   console.log("profileCard:", currentUser);
-  return <div className="profile-card"></div>;
+  return <div className="profile-card">{currentUser.name}</div>;
 }
