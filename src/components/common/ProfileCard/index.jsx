3cd48fe6d2d7ce773c7b662a23d6bd161a07cdf1
Ankit Sharma <sharmaa7861@gmail.com>
2023-04-24 16:59:19 +0530

    Profile edit page completed

--- src/components/common/ProfileCard/index.jsx
+++ src/components/common/ProfileCard/index.jsx
@@ -8,8 +8,19 @@ export default function ProfileCard({ currentUser, onEdit }) {
         <div className="edit-btn">
           <button onClick={onEdit}>Edit</button>
         </div>
-        <h3 className="userName">{currentUser.name}</h3>
-        <p className="userEmail">{currentUser.email}</p>
+
+        <div className="profile-info">
+          <div>
+            <h3 className="userName">{currentUser.name}</h3>
+            <p className="heading">{currentUser.headline}</p>
+            <p>{currentUser.location}</p>
+          </div>
+
+          <div className="right-info">
+            <p className="college">{currentUser.college}</p>
+            <p className="company">{currentUser.company}</p>
+          </div>
+        </div>
       </div>
     </>
   );
