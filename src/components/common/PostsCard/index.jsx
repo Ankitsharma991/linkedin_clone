969394260a96d165432ca9de0f1b53d6fa63d933
Ankit Sharma <sharmaa7861@gmail.com>
2023-05-12 16:10:18 +0530

    post updation and deletion implemented

--- src/components/common/PostsCard/index.jsx
+++ src/components/common/PostsCard/index.jsx
@@ -14,71 +14,76 @@ export default function PostsCard({ posts, id, getEditData }) {
   let navigate = useNavigate();
   const [currentUser, setCurrentUser] = useState({});
   const [allUsers, setAllUsers] = useState([]);
+  const displayUser = () => {
+    console.log(allUsers.filter((user) => user.id === posts.userID)[0]);
+  };
   useMemo(() => {
     getCurrentUser(setCurrentUser);
-    
     getAllUsers(setAllUsers);
   }, []);
   // console.log("PostCard : ", currentUser.id);
-  // console.log("post", posts.userID);\
+  // console.log("post", posts.userID);
 
-  // console.log(
-  //   "single User",
-  //   allUsers.filter((user) => user.id === posts.userID)[0]
-  // );
   // console.log(
   //   "All users",
   //   allUsers.filter((user) => user.id === posts.userID)
   // );
   return (
-    <div className="posts-card" key={id}>
-      <div className="post-image-wrapper">
-        <img
-          className="post-image"
-          src={
-            allUsers
-              .filter((item) => item.id === posts.userID)
-              .map((item) => item.imageLink)[0]
-          }
-          alt="profile-picture"
-        />
-        <div>
-          <p
-            className="name"
-            onClick={() => {
-              navigate("/profile", {
-                state: { id: posts?.id, email: posts.userEmail },
-              });
-            }}
-          >
-            {/* {allUsers.filter((user) => user.id === posts.userID)[0].name} */}
-            {currentUser.name}
-          </p>
-          <p className="timestamp">{posts.timeStamp}</p>
-        </div>
-        {currentUser.id === posts.userID ? (
-          <div className="action-container">
-            <BsPencil
-              size={20}
-              className="action-icon"
-              onClick={() => getEditData(posts)}
+    <>
+      {currentUser && allUsers ? (
+        <div className="posts-card" key={id}>
+          <div className="post-image-wrapper">
+            <img
+              className="post-image"
+              src={
+                allUsers
+                  .filter((item) => item.id === posts.userID)
+                  .map((item) => item.imageLink)[0]
+              }
+              alt="profile-picture"
             />
-            <BsTrash
-              size={20}
-              className="action-icon"
-              onClick={() => deletePost(posts.id)}
-            />
+            <div>
+              <p
+                className="name"
+                onClick={() => {
+                  navigate("/profile", {
+                    state: { id: posts?.id, email: posts.userEmail },
+                  });
+                }}
+              >
+                {allUsers
+                  .filter((item) => item.id === posts.userID)
+                  .map((item) => item.name)[0]}
+              </p>
+              <p className="timestamp">{posts.timeStamp}</p>
+            </div>
+            {currentUser.id === posts.userID ? (
+              <div className="action-container">
+                <BsPencil
+                  size={20}
+                  className="action-icon"
+                  onClick={() => getEditData(posts)}
+                />
+                <BsTrash
+                  size={20}
+                  className="action-icon"
+                  onClick={() => deletePost(posts.id)}
+                />
+              </div>
+            ) : (
+              <></>
+            )}
           </div>
-        ) : (
-          <></>
-        )}
-      </div>
-      <p className="status">{posts.status}</p>
-      <LikeButton
-        userId={currentUser?.id}
-        postId={posts.id}
-        currentUser={currentUser}
-      />
-    </div>
+          <p className="status">{posts.status}</p>
+          <LikeButton
+            userId={currentUser?.id}
+            postId={posts.id}
+            currentUser={currentUser}
+          />
+        </div>
+      ) : (
+        ""
+      )}
+    </>
   );
 }
